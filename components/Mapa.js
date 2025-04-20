6c68eefed952c29e192df60ea2873a404fc2b4d8
Caio171583 <caiovainight@gmail.com>
2025-04-20 16:34:11 -0300

    Add files via upload

---
+++ components/Mapa.js
@@ -0,0 +1,48 @@
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import { useEffect, useState } from "react";
+import L from "leaflet";
+
+export default function Mapa() {
+  const [postes, setPostes] = useState([]);
+  const [selecionados, setSelecionados] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/postes")
+      .then(res => res.json())
+      .then(setPostes);
+  }, []);
+
+  const handleClick = (poste) => {
+    setSelecionados([...selecionados, poste]);
+  };
+
+  const getColor = (empresas) => {
+    const count = empresas?.split(",").length || 0;
+    return count > 5 ? "red" : "green";
+  };
+
+  const customIcon = (color) =>
+    new L.DivIcon({
+      className: "custom-icon",
+      html: `<div style="background-color:${color};width:16px;height:16px;border-radius:50%"></div>`
+    });
+
+  return (
+    <MapContainer center={[-23.71, -46.20]} zoom={13} style={{ height: "100%", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {postes.map((poste, idx) => {
+        const [lat, lon] = poste.coordenadas.split(",").map(Number);
+        const color = getColor(poste.empresa || "");
+        return (
+          <Marker key={idx} position={[lat, lon]} icon={customIcon(color)} eventHandlers={{ click: () => handleClick([lat, lon]) }}>
+            <Popup>
+              <strong>ID:</strong> {poste.id_poste}<br />
+              <strong>Empresas:</strong> {poste.empresa}
+            </Popup>
+          </Marker>
+        );
+      })}
+      {selecionados.length > 1 && <Polyline positions={selecionados} color="blue" />}
+    </MapContainer>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import dynamic from "next/dynamic";
+const Mapa = dynamic(() => import("../components/Mapa"), { ssr: false });
+
+export default function Home() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Mapa />
+    </div>
+  );
+}
