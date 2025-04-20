20790d03ff67ee4940b67086844a74373501a86d
Caio171583 <caiovainight@gmail.com>
2025-04-20 17:36:08 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ const Mapa = dynamic(() => import("../components/Mapa"), { ssr: false });
 
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
       <Mapa />
     </div>
   );
