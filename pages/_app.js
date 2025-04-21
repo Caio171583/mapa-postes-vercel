05f910d32b86ebaac5bcef20e982b99c3887942b
Caio171583 <caiovainight@gmail.com>
2025-04-21 11:10:27 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
+import '../styles/leaflet-cluster/MarkerCluster.Default.css';
+import '../styles/leaflet-cluster/MarkerCluster.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
